c516cfec7a82f771787214fa9a77da74b373d71e
Craig Lenzen <craigl@15mbp.local>
2015-05-21 09:18:27 -0500

    validation_skeleton: Added helper methods failed/succeeded on ValidationRule to make indicating successes and failure a bit easier and abstract

--- validation/src/main/scala/com/payit/components/validation/rules/GeneralOps.scala
+++ validation/src/main/scala/com/payit/components/validation/rules/GeneralOps.scala
@@ -9,8 +9,8 @@ trait GeneralOps {
 
   case class Required[T](allowBlank: Boolean = false) extends ValidationRule[T] {
     def apply(value: T): Validation[RuleViolation, T] = value match {
-      case null => RuleViolation("required", "is required").failure
-      case _ => if (!allowBlank && value.toString.trim.isEmpty) RuleViolation("blank", "must not be blank").failure else value.success
+      case null => failed("required", "is required")
+      case _ => if (!allowBlank && value.toString.trim.isEmpty) failed("blank", "must not be blank") else succeeded(value)
     }
   }
 
@@ -21,7 +21,7 @@ trait GeneralOps {
     require(max >= 0, "max can not be less than ZERO")
 
     def apply(value: T): Validation[RuleViolation, T] = value match {
-      case x if x != null && x.length > max => RuleViolation("maxlength", s"maximum is $max characters", Vector(max.toString)).failure
+      case x if x != null && x.length > max => failed("maxlength", s"maximum is $max characters", Vector(max.toString))
       case _ => value.success
     }
   }
--- validation/src/main/scala/com/payit/components/validation/rules/ValidationRule.scala
+++ validation/src/main/scala/com/payit/components/validation/rules/ValidationRule.scala
@@ -1,5 +1,14 @@
 package com.payit.components.validation.rules
 
-import scalaz.Validation
+import scalaz._
+import Scalaz._
 
-trait ValidationRule[T] extends (T => Validation[RuleViolation, T])
+trait ValidationRule[T] extends (T => Validation[RuleViolation, T]) {
+
+  def succeeded(value: T) = value.success
+
+  def failed(key: String, message: String, params: Vector[String] = Vector.empty[String]) = {
+    RuleViolation(key, message, params).failure
+  }
+
+}
