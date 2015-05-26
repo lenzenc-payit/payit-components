8f04a340124d38d2aac113b7d11450187114b017
Craig Lenzen <craigl@15mbp.local>
2015-05-26 15:28:49 -0500

    validator_macro: Added one simple test spec to ValidatorMacroSpec

--- validation/src/test/scala/com/payit/components/validation/gen/ValidatorMacroSpec.scala
+++ validation/src/test/scala/com/payit/components/validation/gen/ValidatorMacroSpec.scala
@@ -9,11 +9,31 @@ class ValidatorMacroSpec extends Specification {
   case class Person(name: String, age: Int)
 
   ".apply" >> {
-    val validator = validations[Person] { p =>
-      p.name.is(MaxLength(2))
+    "when there is one param with one validation rule" >> {
+      val validator = validations[Person] { p =>
+        p.name.is(Required())
+      }
+      "it should have one rule set with expected one validation rule" >> pending
+      "it should have expected paramName" >> {
+        validator.ruleSets.head.paramName must_== "name"
+      }
     }
-    println(validator(Person("Craig", 39)))
-    success
+    "when there is one param with more than one validation rules" >> {
+      "it should have one rule set with expected two validation rule" >> pending
+      "it should have expected paramName" >> pending
+    }
+    "when there are more than one params with one validation rule" >> {
+      "it should have two rule sets with expected one validation rules each" >> pending
+      "it should have expected paramNames" >> pending
+    }
+    "when there are more than one params with more than one validation rules" >> {
+      "it should have two rule sets with expected two validation rules each" >> pending
+      "it should have expected paramName" >> pending
+    }
+    "when there is code in the mapping closure other than validation expressions" >> {
+      "it should have one rule set with expected one validation rule" >> pending
+      "it should have expected paramName" >> pending
+    }
   }
 
 }
