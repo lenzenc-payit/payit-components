b36f0dda2d2f0c9d8dc50f39307180ed0c3146f4
lenzenc <lenzenc@gmail.com>
2015-05-31 07:48:00 -0500

    common-rules: Added MinLength validation rule

--- validation/src/main/scala/com/payit/components/validation/rules/GeneralOps.scala
+++ validation/src/main/scala/com/payit/components/validation/rules/GeneralOps.scala
@@ -5,11 +5,38 @@ import Scalaz._
 
 trait GeneralOps {
 
+  type HasLength = Any { def length: Int }
+
   case class Required[T](allowBlank: Boolean = false) extends ValidationRule[T] {
     def apply(value: T): Validation[RuleViolation, T] = value match {
       case null => failed("required", "is required")
       case _ => if (!allowBlank && value.toString.trim.isEmpty) failed("blank", "must not be blank") else succeeded(value)
     }
+  }
+
+  case class MaxLength[T <% HasLength](max: Int) extends ValidationRule[T] {
+
+    import scala.language.reflectiveCalls
+
+    require(max >= 0, "max can not be less than ZERO")
+
+    def apply(value: T): Validation[RuleViolation, T] = value match {
+      case x if x != null && x.length > max => failed("maxlength", s"maximum is $max characters", Vector(max.toString))
+      case _ => succeeded(value)
+    }
+  }
+
+  case class MinLength[T <% HasLength](min: Int) extends ValidationRule[T] {
+
+    import scala.language.reflectiveCalls
+
+    require(min >= 0, "min can not be less than ZERO")
+
+    def apply(value: T): Validation[RuleViolation, T] = value match {
+      case x if x == null || (x != null && x.length < min) => failed("minlength", s"minimum is $min characters", Vector(min.toString))
+      case _ => succeeded(value)
+    }
+
   }
 
 }
