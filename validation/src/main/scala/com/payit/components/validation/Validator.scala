2d9f54f5e247e921cf486a2cce5cbb7b3f29fd12
Craig Lenzen <craigl@15mbp.local>
2015-06-12 08:47:30 -0500

    Updated ValidatorSpec to work with new refactored validation structure

--- validation/src/main/scala/com/payit/components/validation/Validator.scala
+++ validation/src/main/scala/com/payit/components/validation/Validator.scala
@@ -2,28 +2,30 @@ package com.payit.components.validation
 
 case class FailureKey(key: String, parentKey: ParentKey = ParentKey())
 
-trait Validator[T] extends (T => Validated[Map[FailureKey, ValidationFailure], T]) {
+trait Validator[T] extends (T => Validated[Map[FailureKey, Seq[ValidationFailure]], T]) {
 
   def ruleSets: Seq[ValidationRuleSet[T, _]]
 
-  def apply(obj: T): Validated[Map[FailureKey, ValidationFailure], T] = {
-    Success(obj)
-//    var failures = Seq[ValidationFailure]()
-//    ruleSets.foreach { ruleSet =>
-//      ruleSet(obj) match {
-//        case Success(_) => Nil
-//        case Fail(f) => {
-//          failures = failures ++ f
-//        }
-//      }
-//
-//    }
-//    if (failures.isEmpty) Success(obj) else Fail(failures)
+  def apply(obj: T): Validated[Map[FailureKey, Seq[ValidationFailure]], T] = {
+    var failures = Map[FailureKey, Seq[ValidationFailure]]()
+    ruleSets.foreach { ruleSet =>
+      ruleSet(obj) match {
+        case Success(_) => Nil
+        case Fail(f) => {
+          failures = failures + (
+            FailureKey(ruleSet.key, ruleSet.parentKey) ->
+            concat(failures.getOrElse(FailureKey(ruleSet.key, ruleSet.parentKey), Seq.empty[ValidationFailure]), f)
+          )
+        }
+      }
+    }
+
+    if (failures.isEmpty) Success(obj) else Fail(failures)
   }
 
   private def concat(v1: Seq[ValidationFailure], v2: Seq[ValidationFailure]): Seq[ValidationFailure] = {
-    val keys = v1.map{ f => f.parentKey.toString + f.key}
-    v1 ++ v2.filter { f => !keys.contains(f.parentKey.toString + f.key) }
+    val ruleKeys = v1.map{ f => f.ruleKey}
+    v1 ++ v2.filter { f => !ruleKeys.contains(f.ruleKey) }
   }
 
 }
